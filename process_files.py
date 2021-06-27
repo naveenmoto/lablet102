ae65f5d8287fc300f3abc546d512c41078867216
Naveen <naveen@atimotors.com>
2021-06-27 20:33:26 +0530

    week 1 submissions

---
+++ process_files.py
@@ -0,0 +1,16 @@
+import os, rarfile
+def unrar():
+	with open("./rarlist", "r") as f:
+		rar_files = f.readlines()
+	for fname in rar_files:
+		os.system("wget " + fname)
+	os.system("mv *.rar week1/")
+	for rf in os.listdir("./week1"):
+		f = os.path.join("./week1",rf)
+		o = rarfile.RarFile(f)
+		for u in o.infolist():
+			print (u.filename, u.file_size)
+			o.extractall("./week1")
+	
+if __name__ == "__main__":
+	unrar()
