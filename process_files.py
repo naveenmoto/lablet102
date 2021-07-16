1e90adc093bb56d4487342029682c77d76922452
Naveen <naveen@artpark.in>
2021-07-16 08:46:34 +0530

    week 3 submissions

--- process_files.py
+++ process_files.py
@@ -4,13 +4,14 @@ def unrar():
 		rar_files = f.readlines()
 	for fname in rar_files:
 		os.system("wget " + fname)
-	os.system("mv *.rar week2/")
-	for rf in os.listdir("./week2"):
-		f = os.path.join("./week2",rf)
-		o = rarfile.RarFile(f)
-		for u in o.infolist():
-			print (u.filename, u.file_size)
-			o.extractall("./week2")
+	os.system("mv *.rar week3/")
+
+	#for rf in os.listdir("./week3"):
+	#	f = os.path.join("./week3",rf)
+	#	o = rarfile.RarFile(f)
+	#	for u in o.infolist():
+	#		print (u.filename, u.file_size)
+	#		o.extractall("./week3")
 	
 if __name__ == "__main__":
 	unrar()
